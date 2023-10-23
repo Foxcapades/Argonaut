93470d0bebb56ec3dde63be1b113bf084981d39a
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-10-22 21:48:02 -0400

    update readme

--- examples/complex-type/main.go
+++ examples/complex-type/main.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
 	cli "github.com/Foxcapades/Argonaut"
 )
 
 type Inputs struct {
-	Map map[string][]string
+	Strings       []string
+	IntToBool     map[int]bool
+	StringToBytes map[string]*[]byte
 }
 
 func main() {
 	var conf Inputs
 
-	cli.Command().
+	com := cli.Command().
 		WithFlag(cli.Flag().
+			WithLongForm("string-slice").
 			WithShortForm('s').
-			WithBinding(&conf.Map, true)).
+			WithBinding(&conf.Strings, true)).
+		WithFlag(cli.Flag().
+			WithLongForm("int-bool-map").
+			WithShortForm('i').
+			WithBinding(&conf.IntToBool, true)).
+		WithFlag(cli.Flag().
+			WithLongForm("string-bytes").
+			WithShortForm('b').
+			WithBinding(&conf.StringToBytes, true)).
 		MustParse(os.Args)
+
+	fmt.Println(com.UnmappedInputs())
 
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
